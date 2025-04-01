634ed7a0cfbd760e9d0421faa149fe8b2ecfb651
VanilasPage <vanilas.help@gmail.com>
2025-04-01 14:43:15 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,32 +1,47 @@
+const body = document.body
+const headerMenuLi = document.querySelectorAll(`li`)
 const logoBtn = document.querySelector(`#logo`)
-console.log(logoBtn);
-
 const copyIpBtn = document.querySelector(`#copyIp`)
-console.log(copyIpBtn);
-
 const themeBtn = document.querySelector(`#theme`)
-console.log(themeBtn);
-
+const themeIcon = document.querySelector(`#theme > svg`)
 const othernewsBtn = document.querySelector(`#othernews`)
-console.log(othernewsBtn);
-
 const hMainBtn = document.querySelector(`#hMain`)
-console.log(hMainBtn);
-
 const hPeculiaritiesBtn = document.querySelector(`#hPeculiarities`)
-console.log(hPeculiaritiesBtn);
-
 const hRulesBtn = document.querySelectorAll(`#hRules`)
-console.log(hRulesBtn);
-
 const hDonateBtn = document.querySelector(`#hDonate`)
-console.log(hDonateBtn);
-
 const hTgBtn = document.querySelector(`#hTg`)
-console.log(hTgBtn);
-
 const hDiscordBtn = document.querySelector(`#hDiscord`)
-console.log(hDiscordBtn);
+
+console.dir(themeIcon)
+
+if (localStorage.getItem(`theme`)==undefined) {
+    localStorage.setItem(`theme`,`light`)
+} else {
+    localStorage.setItem(`theme`,(localStorage.getItem(`theme`)))
+}
+function theme() {
+    if (localStorage.getItem(`theme`)==`light`) {
+        body.style.background = `url("./files/BackgroundDark.png")`
+        body.style.backgroundSize = `cover`
+        body.style.color = `white`
+        headerMenuLi.forEach(li =>{
+            li.style.color = `white`
+        })
+        themeIcon.style.color = `white`
+        themeIcon.innerHTML = `<path d="M6 .278a.77.77 0 0 1 .08.858 7.2 7.2 0 0 0-.878 3.46c0 4.021 3.278 7.277 7.318 7.277q.792-.001 1.533-.16a.79.79 0 0 1 .81.316.73.73 0 0 1-.031.893A8.35 8.35 0 0 1 8.344 16C3.734 16 0 12.286 0 7.71 0 4.266 2.114 1.312 5.124.06A.75.75 0 0 1 6 .278"/>`
+    } else if (localStorage.getItem(`theme`)==`dark`) {
+        body.style.background = `url("./files/BackgroundLight.jpg")`
+        body.style.backgroundSize = `cover`
+        body.style.color = `black`
+        headerMenuLi.forEach(li =>{
+            li.style.color = `black`
+        })
+        themeIcon.style.color = `black`
+        themeIcon.innerHTML = `<path d="M12 8a4 4 0 1 1-8 0 4 4 0 0 1 8 0zM8 0a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 0zm0 13a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 13zm8-5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2a.5.5 0 0 1 .5.5zM3 8a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2A.5.5 0 0 1 3 8zm10.657-5.657a.5.5 0 0 1 0 .707l-1.414 1.415a.5.5 0 1 1-.707-.708l1.414-1.414a.5.5 0 0 1 .707 0zm-9.193 9.193a.5.5 0 0 1 0 .707L3.05 13.657a.5.5 0 0 1-.707-.707l1.414-1.414a.5.5 0 0 1 .707 0zm9.193 2.121a.5.5 0 0 1-.707 0l-1.414-1.414a.5.5 0 0 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .707zM4.464 4.465a.5.5 0 0 1-.707 0L2.343 3.05a.5.5 0 1 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .708z"/>`
+    }
+}
+theme()
+
 
 logoBtn.addEventListener(`click`,(event)=>{
     location.reload();
@@ -41,7 +56,13 @@ copyIpBtn.addEventListener(`click`,(event)=>{
 })
 
 themeBtn.addEventListener(`click`,(event)=>{
-    alert(`Изменения темы пока в разработке!`)
+    if (localStorage.getItem(`theme`)==`light`) {
+        localStorage.setItem(`theme`,`dark`)
+        theme()
+    } else if (localStorage.getItem(`theme`)==`dark`) {
+        localStorage.setItem(`theme`,`light`)
+        theme()
+    }
 })
 
 othernewsBtn.addEventListener(`click`,(event)=>{
